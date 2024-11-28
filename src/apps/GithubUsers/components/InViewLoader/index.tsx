62d9b97649241f2748a6ebda637004e5268f616d
Fernando EM <vs_fernando@yahoo.fr>
2024-11-28 11:19:56 +0100

    InViewLoader - fetch more users after reaching the the bottom of the list

---
+++ src/apps/GithubUsers/components/InViewLoader/index.tsx
@@ -0,0 +1,36 @@
+import { Box } from "@mui/material";
+import { useInView } from "react-intersection-observer";
+
+interface Props {
+  error?: boolean;
+  hasMore?: boolean;
+  loadMore?: () => void;
+  isLoading?: boolean;
+}
+/** @description this component must execute the `loadMore` callback when it's in view */
+export const InViewLoader = ({
+  hasMore,
+  loadMore,
+  isLoading,
+  error,
+}: Props) => {
+  const [anchor, isInView] = useInView({
+    // rootMargin: "0px 0px -10px 0px",
+    threshold: 0,
+    onChange: (inView) => {
+      if (!inView) return;
+      if (isLoading) return;
+      if (!hasMore) return;
+
+      loadMore?.();
+    },
+  });
+
+  const canBeShown = hasMore && !error;
+
+  return (
+    <Box minHeight={canBeShown ? 40 : 0} ref={anchor} textAlign="center">
+      {canBeShown ? "Loading..." : ""}
+    </Box>
+  );
+};
--- src/apps/GithubUsers/containers/RootContainer/index.tsx
+++ src/apps/GithubUsers/containers/RootContainer/index.tsx
@@ -5,6 +5,7 @@ import { useState } from "react";
 import { useGetUsersList } from "../../../../libs/api/hooks/useGetUsersList";
 import { UserSearchForm } from "../../components/UserSearchForm";
 import { UserSearchItem } from "../UserItemContainer";
+import { InViewLoader } from "../../components/InViewLoader";
 
 export const GithubUser = () => {
   const [search, setUsername] = useState("");
@@ -19,19 +20,32 @@ export const GithubUser = () => {
   const resultCaption = `${totalCount} results for ${search}`;
 
   return (
-    <Container sx={{ height: "100vh" }}>
-      <Stack width={540} maxWidth={1} mx="auto" gap={2}>
+    <Container>
+      <Stack
+        height="100vh"
+        width={540}
+        maxWidth={1}
+        mx="auto"
+        gap={2}
+        overflow="auto"
+      >
         <UserSearchForm
           isLoading={isLoading}
           onSubmit={setUsername}
           errorMessage={doGetUsersList.error?.message}
         />
         {!!search && <Alert>{resultCaption}</Alert>}
-        <Stack>
+        <Stack px={2}>
           {usersList?.map((user) => (
             <UserSearchItem key={user.id} user={user} />
           ))}
         </Stack>
+        <InViewLoader
+          hasMore={doGetUsersList.hasNextPage}
+          isLoading={isLoading}
+          loadMore={doGetUsersList.fetchNextPage}
+          error={!!doGetUsersList.error}
+        />
       </Stack>
     </Container>
   );
