919c4c1d7131a344f0b9c7e5bab714d4fb71fc1f
Fernando EM <vs_fernando@yahoo.fr>
2024-11-28 17:32:19 +0100

    clean up

--- src/libs/api/utils/getEnvironmentVariables.ts
+++ src/libs/api/utils/getEnvironmentVariables.ts
@@ -1,8 +1,4 @@
-import { unstable_noStore as noStore } from "next/cache";
-
 export default function getEnvVariable(name: string) {
-  // Using noStore prevents this function from being rendered statically
-  noStore();
   const variable = process.env[name];
   if (!variable) {
     throw new Error("Missing environment variable for " + name);
