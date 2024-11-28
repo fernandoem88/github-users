efb4d90a29283f86416f92b4cd3698a37d381741
Fernando EM <vs_fernando@yahoo.fr>
2024-11-28 11:15:20 +0100

    global css clean up

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { AppProviders } from "../Providers/AppProviders";
+import "./globals.css";
 
 export const metadata: Metadata = {
   title: "Create Next App",
