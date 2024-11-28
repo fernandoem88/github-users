b6c6bcea11b696cdba438fcb03bb8e879a7b903f
Fernando EM <vs_fernando@yahoo.fr>
2024-11-28 11:20:57 +0100

    parsing error message

--- src/libs/api/utils/index.ts
+++ src/libs/api/utils/index.ts
@@ -2,6 +2,8 @@ export const throwResponseError = async (response: Response) => {
   const { ok, statusText, status } = response;
   if (!ok || status >= 400) {
     const data = await response.json();
-    throw { message: data.message, status, statusText };
+
+    const message = data.message || statusText;
+    throw new Error(`${status} Error: ${message}`);
   }
 };
